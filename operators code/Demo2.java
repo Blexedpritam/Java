4a5c239f02b0c1a7b3563180fcb4f954f215f3f3
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-06-30 21:57:48 +0530

    Relational and Bitwise Operators eg.

---
+++ operators code/Demo2.java
@@ -0,0 +1,19 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        //Relational Operators . -->  == , != , < , > , <= , >=  
+       
+        int a = 5 ;
+        int b = 10;
+        
+        boolean c = ( a == b); // Flase
+        System.out.println(c);
+
+        boolean d = (a != b);  // True 
+        System.out.println(d);
+
+        boolean e  = (a <= b); // True 
+        boolean f = (a > b); // Flase
+
+        System.out.println( e + " ," + f);
+    }
+}
