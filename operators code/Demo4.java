5f9f026cb17f54ff47b17e6f8ae1946427e4da8d
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-07-09 22:05:24 +0530

    Shift and Logical Operators

---
+++ operators code/Demo4.java
@@ -0,0 +1,14 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        // Logical Operators
+
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        boolean d = (a < b) && (b < c);
+
+        //Short Circuit 
+
+        System.out.println(d); // True  
+    }
+}
