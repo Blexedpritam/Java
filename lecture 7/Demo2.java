cd2cecb772974b332eb3878ad418813727faf503
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-08-01 20:24:00 +0530

    Practice Pending

--- lecture 7/Demo2.java
+++ lecture 7/Demo2.java
@@ -3,7 +3,7 @@ public class Demo2 {
         //Switch Statements
         int  i = 3;
 
-        switch (i){
+        switch (i){ // switch(expression)-->byte,short,char,Enumeration
             case 1:
                 System.out.println("i is 1");
                 break;
@@ -17,5 +17,25 @@ public class Demo2 {
                 System.out.println("i is greater than 2");   
                 break;
         }
+    //Switch--> limited case --> should evalute to byte,short,int,char,Enumeration
+    // no dupicate cases allowed.
+    //After JDK7 strings can aslo be used as switch expression
+    //After JDK-14 switch is enhanced to much.
+
+        int j = 5;
+        switch(j){
+            case 4:
+                System.out.println("j is not 4");
+                break;
+            case 5:
+                System.out.println("j is 5");
+                break;
+                //Nested Switch Possible
+            default:
+                System.out.println("j is not 4 & 5");
+        }
+
     }
 }
+
+
---
+++ lecture 7/Practice Condition/PostiveNumberCheck.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class PostiveNumberCheck {
+    public static void main(String[] args) {
+        //Createing a Scanner Object
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Postive number :");
+        int n = scanner.nextInt();
+
+        if(n>0){
+           
+        }
+        System.out.println("Postive Number is :" + n);
+        scanner.close();
+    }
+    
+}
