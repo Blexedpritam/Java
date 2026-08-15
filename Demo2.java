337f02a332a4ecbc41491f171c28543a3e2b2020
pritamkumarpatna59@gmail.com <pritamkumarpatna@gmail.com>
2026-08-15 19:15:52 +0530

    Encapsulation , Inheritance and Packages

---
+++ Demo2.java
@@ -0,0 +1,20 @@
+//import college.Student;
+// import school.Student;
+
+// import school.*;
+// import college.*;
+
+//Syntax : import packageName.className
+
+public class Demo2 {
+    public static void main(String[] args) {
+        college.Student s1 = new college.Student();
+        s1.print();
+
+        school.Student s2 = new school.Student();
+        s2.print();
+
+        
+        
+    }
+}
