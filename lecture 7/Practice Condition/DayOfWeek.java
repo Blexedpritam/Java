2199b656069f29bfbf185dc8b35cd6cc5e289ed7
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-09-04 20:08:18 +0530

    Add Lecture 07 Backup Codes

---
+++ lecture 7/Practice Condition/DayOfWeek.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Number (1 - 7):");
+        int num = scanner.nextInt();
+
+        switch(num){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            
+        }
+        scanner.close();
+    }
+}
---
+++ lecture 7/Practice Condition/NumberSignCheck.java
@@ -0,0 +1,15 @@
+public class NumberSignCheck {
+    public static void main(String[] args) {
+        int numberSign = 0;
+
+        if(numberSign > 0){
+            System.out.println("Postive Number");
+        }
+        else if(numberSign < 0){
+            System.out.println("Negative Number");
+        }
+        else{
+            System.out.println("Zero");
+        }
+    }
+}
