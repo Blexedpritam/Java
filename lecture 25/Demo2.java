2bfef57469c4ac55a41c5298a261b37a2009d977
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-09-06 17:44:42 +0530

    Understand String Immutability and Internals

---
+++ lecture 25/Demo2.java
@@ -0,0 +1,11 @@
+//Problem Of Immutability 
+public class Demo2 {
+    public static void main(String[] args) {
+        String s = "";
+
+        for(int i = 0 ; i < 5; i++){
+            s += i;
+            System.out.println(s);
+        }
+    }
+}
