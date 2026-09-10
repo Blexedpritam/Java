9705dfbae86cc20449e2cfc11ea4e8dcecb14a48
Blexedpritam <pritamkumarpatna59@gmail.com>
2026-09-10 21:44:26 +0530

    Comment out Box<Animal> instantiation in Demo7.java

--- lecture 27/Demo7.java
+++ lecture 27/Demo7.java
@@ -2,7 +2,7 @@ public class Demo7 {
     public static void main(String[] args) {
         // Box<Fish> box = new Box<>();
 
-        Box<Animal> box = new Box<>();
+        // Box<Animal> box = new Box<>();
 
 
     }
